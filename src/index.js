be086a1a884ba78885bb95b6c2e6cfe4acbadf92
dizollate <66797250+dizollate@users.noreply.github.com>
2021-02-28 18:49:08 +0300

    feat: add fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
- let newMassive = [];
+	if (matrix.length == 0){
+    [];
+    }
+ 	let newMassive = [];
     let len = matrix.length;
     for (let i = 0; i < len; i = i+1){
       if (i % 2 == 0){
