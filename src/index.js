10806fc262e5315f1306658e9143eb26e5067377
dizollate <66797250+dizollate@users.noreply.github.com>
2021-02-28 18:45:03 +0300

    feat: add fix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
- return matrix.flat(matrix.length);
+ let newMassive = [];
+    let len = matrix.length;
+    for (let i = 0; i < len; i = i+1){
+      if (i % 2 == 0){
+      	newMassive.push(matrix[i]);
+      } else {
+      	newMassive.push(matrix[i].reverse());
+      }
+    }
+   return newMassive.flat(len);
 }
